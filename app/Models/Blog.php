2a48807697bf813666e7ece37dd9f5da7507aa29
Nicholas Bell <nick@advantagelearn.com>
2026-01-18 22:18:16 +0200

    Add HasSlug trait to Blog model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\BlogStatus;
+use App\Models\Concerns\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,7 @@ class Blog extends Model
 {
     /** @use HasFactory<\Database\Factories\BlogFactory> */
     use HasFactory;
+    use HasSlug;
 
     /**
      * @var list<string>
