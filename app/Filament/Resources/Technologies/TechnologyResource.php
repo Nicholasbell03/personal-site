a0a09eb2167d5102774d3198c0ecb3a2c8d750ec
Nicholas Bell <nick@advantagelearn.com>
2026-01-25 23:53:17 +0200

    feat: add Technology resource with CRUD pages and table configuration

---
+++ app/Filament/Resources/Technologies/TechnologyResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources\Technologies;
+
+use App\Filament\Resources\Technologies\Pages\CreateTechnology;
+use App\Filament\Resources\Technologies\Pages\EditTechnology;
+use App\Filament\Resources\Technologies\Pages\ListTechnologies;
+use App\Filament\Resources\Technologies\Tables\TechnologiesTable;
+use App\Models\Technology;
+use BackedEnum;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Set;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+
+class TechnologyResource extends Resource
+{
+    protected static ?string $model = Technology::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCpuChip;
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Technology Details')
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(function (Set $set, ?string $state, $record) {
+                                if (! $record) {
+                                    $set('slug', Str::slug($state ?? ''));
+                                }
+                            }),
+                        TextInput::make('slug')
+                            ->required()
+                            ->readOnly(fn ($record) => ! $record)
+                            ->unique(ignoreRecord: true),
+                    ])
+                    ->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return TechnologiesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListTechnologies::route('/'),
+            'create' => CreateTechnology::route('/create'),
+            'edit' => EditTechnology::route('/{record}/edit'),
+        ];
+    }
+}
