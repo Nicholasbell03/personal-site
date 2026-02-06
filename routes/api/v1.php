26217bde6ad4b3f4e93d737f47ae88c50e337b50
Nicholas Bell <nick@advantagelearn.com>
2026-02-06 12:30:23 +0200

    feat: add routes for ShareController in API v1
    - Introduced new routes for the ShareController, including index, featured, and show endpoints.
    - Added authentication middleware for the store endpoint to manage shares securely.

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\BlogController;
 use App\Http\Controllers\Api\V1\ProjectController;
+use App\Http\Controllers\Api\V1\ShareController;
 use App\Http\Middleware\ValidatePreviewToken;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,14 @@ Route::prefix('projects')->group(function () {
     Route::get('/featured', [ProjectController::class, 'featured'])->name('v1.projects.featured');
     Route::get('/preview/{slug}', [ProjectController::class, 'preview'])->name('v1.projects.preview')->middleware(ValidatePreviewToken::class);
     Route::get('/{slug}', [ProjectController::class, 'show'])->name('v1.projects.show');
+});
+
+Route::prefix('shares')->group(function () {
+    Route::get('/', [ShareController::class, 'index'])->name('v1.shares.index');
+    Route::get('/featured', [ShareController::class, 'featured'])->name('v1.shares.featured');
+    Route::get('/{slug}', [ShareController::class, 'show'])->name('v1.shares.show');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/shares', [ShareController::class, 'store'])->name('v1.shares.store');
 });
